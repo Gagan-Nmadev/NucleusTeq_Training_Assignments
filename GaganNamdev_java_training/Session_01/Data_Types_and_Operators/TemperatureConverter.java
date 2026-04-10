1a1270744f202ee84e4ea102333ed2292742f327
Gagan Namdev <gagancomputer98@gmail.com>
2026-04-10 15:38:29 +0530

    Added method to convert Celsius to Fahrenheit using formula

--- GaganNamdev_java_training/Session_01/Data_Types_and_Operators/TemperatureConverter.java
+++ GaganNamdev_java_training/Session_01/Data_Types_and_Operators/TemperatureConverter.java
@@ -1,3 +1,11 @@
 public class TemperatureConverter {
     
+    //  Celsius to Fahrenheit conversion
+    public void celsiusToFahrenheit(double celsius) {
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println("Celsius to Fahrenheit:");
+        System.out.println(celsius + " °C = " + fahrenheit + " °F");
+    }
 }
