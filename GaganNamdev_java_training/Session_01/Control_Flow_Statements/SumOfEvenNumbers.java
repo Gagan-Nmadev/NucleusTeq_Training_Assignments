ec8e42babd14e5e422a6b49f834031c550919009
Gagan Namdev <gagancomputer98@gmail.com>
2026-04-10 16:10:03 +0530

    Added variables for loop counter and sum initialization

--- GaganNamdev_java_training/Session_01/Control_Flow_Statements/SumOfEvenNumbers.java
+++ GaganNamdev_java_training/Session_01/Control_Flow_Statements/SumOfEvenNumbers.java
@@ -1,3 +1,11 @@
 public class SumOfEvenNumbers {
+
+     public static void main(String[] args) {
+
+        //  starting number
+        int i = 1;
+
+        //  variable to store sum of even numbers
+        int sum = 0;
     
 }
