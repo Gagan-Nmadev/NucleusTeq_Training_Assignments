e3c4826475be297e67f8849a42876d7ce109fb93
Gagan Namdev <gagancomputer98@gmail.com>
2026-04-10 16:27:12 +0530

    Added array programs including average of elements

---
+++ GaganNamdev_java_training/Session_01/Arrays/ArrayAverage.java
@@ -0,0 +1,18 @@
+// package NucleusTeq_Training_Assignments.GaganNamdev_java_training.Session_01.Arrays;
+
+import java.util.Scanner;
+
+public class ArrayAverage {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //  size of array input
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n]; //  array declaration
+
+        int sum = 0;
+
+}
