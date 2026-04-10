30664bfb4a3c15210a528bcb8522279aba07ac22
Gagan Namdev <gagancomputer98@gmail.com>
2026-04-10 16:01:19 +0530

    Implemented user input logic for three integers using nextInt()

--- GaganNamdev_java_training/Session_01/Control_Flow_Statements/LargestNumber.java
+++ GaganNamdev_java_training/Session_01/Control_Flow_Statements/LargestNumber.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class LargestNumber {
+
+    public static void main(String[] args) {
+
+        //  Scanner object for user input
+        Scanner sc = new Scanner(System.in);
+
+        //  Taking input from user
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+
+        int largest;
     
 }
