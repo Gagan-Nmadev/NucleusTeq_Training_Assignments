6b3ebafdbf6bda35f0869f4cb9e15f0d2b60a9cd
Gagan Namdev <gagancomputer98@gmail.com>
2026-04-19 23:37:35 +0530

    Added TodoDTO with validation annotations

--- GaganNamdev_java_training/Session_01/java/session4/todoapp/todoapp/src/main/java/com/training/todoapp/dto/TodoDTO.java
+++ GaganNamdev_java_training/Session_01/java/session4/todoapp/todoapp/src/main/java/com/training/todoapp/dto/TodoDTO.java
@@ -1,4 +1,44 @@
 package com.training.todoapp.dto;
 
+import com.training.todoapp.entity.Todo.Status;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
+import jakarta.validation.constraints.NotBlank;
+
 public class TodoDTO {
+
+    // DTO controls input data and prevents direct exposure of entity
+
+    private Long id;
+
+    @NotBlank(message = "Title cannot be blank")
+    @Size(min = 3, message = "Title must be at least 3 characters")
+    private String title;
+
+    private String description;
+
+
+    private Status status;// Optional → defaults to PENDING in backend
+
+    private LocalDateTime createdAt;
+
+    public TodoDTO() {}
+
+    // Getters & Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Status getStatus() { return status; }
+    public void setStatus(Status status) { this.status = status; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
