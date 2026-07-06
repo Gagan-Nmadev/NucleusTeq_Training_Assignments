ba5f8a7c51e4406b1796bc156807d4126961b027
Gagan Namdev <gagancomputer98@gmail.com>
2026-07-07 02:16:28 +0530

    Implemented Issue Workflow

--- Gagan_Namdev_Capstone_Issue_Sprint_Management_System/backend/app/api/routers/issue_router.py
+++ Gagan_Namdev_Capstone_Issue_Sprint_Management_System/backend/app/api/routers/issue_router.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends
 
-from app.schemas.issue_schema import IssueCreate
+from app.schemas.issue_schema import (
+    IssueCreate,
+    IssueStatusUpdate,
+)
 from app.services.issue_service import IssueService
 from app.api.dependencies.auth import get_current_admin
 
@@ -17,3 +20,15 @@ def create_issue(
     current_admin=Depends(get_current_admin)
 ):
     return IssueService.create_issue(issue)
+
+
+@router.put("/{issue_id}/status")
+def update_issue_status(
+    issue_id: str,
+    status: IssueStatusUpdate,
+    current_admin=Depends(get_current_admin)
+):
+    return IssueService.update_issue_status(
+        issue_id,
+        status
+    )
